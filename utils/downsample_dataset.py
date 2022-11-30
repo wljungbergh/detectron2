171d709c8fd945455e8da247b6b3758ab7637d57
William Ljungbergh <william.ljungbergh@zenseact.com>
2022-11-30 11:29:43 +0100

    add more downsampling

--- utils/downsample_dataset.py
+++ utils/downsample_dataset.py
@@ -65,14 +65,15 @@ def downsample_and_store_raw(image_path: str):
     filename = os.path.basename(image_path).replace(".nef", ".npy")
     new_filename = os.path.join(folder, filename)
 
-    if os.path.exists(new_filename):
-        return
+    # if os.path.exists(new_filename):
+    #    return
 
     with rawpy.imread(image_path) as raw:
         # make sure that it has the same size as jpg without removing any partial bayer patterns
         raw_image = raw.raw_image_visible
 
         raw_image_downsampled = downsample_raw_image(raw_image, scale=SCALE)
+
         # store as numpy array
 
         os.makedirs(folder, exist_ok=True)
@@ -80,10 +81,38 @@ def downsample_and_store_raw(image_path: str):
         np.save(new_filename, raw_image_downsampled)
 
 
+def postproccess_raw_to_jpg(image_path: str):
+    downsample_folder = os.path.dirname(image_path).replace("full", "fifth")
+    downsample_filename = os.path.basename(image_path).replace(".nef", ".npy")
+    downsample_filename = os.path.join(downsample_folder, downsample_filename)
+    downsampled_raw = np.load(downsample_filename)
+
+    new_folder = (
+        os.path.dirname(image_path)
+        .replace("full", "fifth")
+        .replace("fifth/raw", "fifth/jpg_downsampled")
+    )
+    new_filename = os.path.basename(image_path).replace(".nef", ".jpg")
+    new_filename = os.path.join(new_folder, new_filename)
+
+    if os.path.exists(new_filename):
+        return
+
+    with rawpy.imread(image_path) as raw:
+        # make sure that it has the same size as jpg without removing any partial bayer patterns
+        for h in range(DOWNSAMPLED_HEIGHT):
+            for w in range(DOWNSAMPLED_WIDTH):
+                raw.raw_image_visible[h, w] = downsampled_raw[h, w]
+
+        rgb = raw.postprocess()[:DOWNSAMPLED_HEIGHT, :DOWNSAMPLED_WIDTH, :]
+
+    os.makedirs(new_folder, exist_ok=True)
+    os.chmod(new_folder, 0o777)
+
+    Image.fromarray(rgb).save(new_filename)
+
+
 def downsample_raw_image(image: np.ndarray, scale: int = 5):
-    # split image into 6x6 blocks
-    # downsample each block
-    # store in new image
     new_image = np.zeros(
         (image.shape[0] // scale, image.shape[1] // scale), dtype=np.uint16
     )
@@ -95,6 +124,35 @@ def downsample_raw_image(image: np.ndarray, scale: int = 5):
                 kernel
             )
     return new_image
+
+
+def get_linear_raw(image_path: str):
+
+    folder = (
+        os.path.dirname(image_path)
+        .replace("full", "fifth")
+        .replace("fifth/raw", "fifth/linear_raw")
+    )
+    filename = os.path.basename(image_path).replace(".nef", ".npy")
+    new_filename = os.path.join(folder, filename)
+
+    if os.path.exists(new_filename):
+        return
+
+    os.makedirs(folder, exist_ok=True)
+    os.chmod(folder, 0o777)
+
+    with rawpy.imread(image_path) as raw:
+        # make sure that it has the same size as jpg without removing any partial bayer patterns
+        rgb = raw.postprocess(gamma=(1, 1), no_auto_bright=True, output_bps=16)
+        new_image = np.zeros((rgb.shape[0], rgb.shape[1]), dtype=np.uint16)
+        new_image[::2, ::2] = rgb[::2, ::2, 0]  # red
+        new_image[1::2, 1::2] = rgb[1::2, 1::2, 2]  # blue
+        new_image[::2, 1::2] = rgb[::2, 1::2, 1]  # green
+        new_image[1::2, ::2] = rgb[1::2, ::2, 1]  # green
+
+        downsampled_linear = downsample_raw_image(new_image, scale=SCALE)
+        np.save(new_filename, downsampled_linear)
 
 
 def main():
@@ -114,6 +172,8 @@ def main():
 
     process_map(downsample_and_store_jpeg, jpg_files, max_workers=None)
     process_map(downsample_and_store_raw, raw_files, max_workers=None)
+    process_map(postproccess_raw_to_jpg, raw_files, max_workers=None)
+    process_map(get_linear_raw, raw_files, max_workers=None)
 
 
 if __name__ == "__main__":
